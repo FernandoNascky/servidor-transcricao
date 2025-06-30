3ac0986896184440d81d012950999ebb55cbf837
FernandoNascky <gilson.fernando.mv2@gmail.com>
2025-06-30 01:07:47 -0300

    Update server.py
    teste

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Substitua pela sua chave da OpenAI
-openai.api_key = "SUA_CHAVE_OPENAI"
+openai.api_key = "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"
 
 @app.route("/transcrever", methods=["POST"])
 def transcrever_audio():
